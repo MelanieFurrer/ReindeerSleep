e093142f86e6f69a8990e004d203c0d56343b4b1
MelanieFurrer <73651373+MelanieFurrer@users.noreply.github.com>
2022-12-08 09:42:00 +0100

    Create B_Calc_align_SWA_rem_L.m
    added script to calculate SWA during REM sleep

---
+++ 0B_Calc_SWA/B_Calc_align_SWA_rem_L.m
@@ -0,0 +1,195 @@
+clear
+close all
+
+path='D:\Work\reindeer\Analyse_mainexperiment\Data\';
+datapath0=[path,'Mat_power_artcorr_scored_cut\Adapt'];
+datapath1=[path,'Mat_power_artcorr_scored_cut\BL1'];
+datapath2=[path,'Mat_power_artcorr_scored_cut\SR_BL2'];
+savepath='C:\Users\schlaf\Documents\reindeer\Data_Analysis_main_experiment\Results\SWA_normalized_aligned\';
+
+
+cd(datapath2)
+filenames2=dir('*power*.mat');
+nfiles=length(filenames2)
+
+for i=1:nfiles
+
+    if i==1  
+        
+       %adapt (only first 12 hours there), shift data according to starttime 14h
+        cd(datapath0)
+        filenames0=dir('*power*.mat')
+        filename=filenames0(i).name;
+        load(filename);       
+        powerEEG_pcut_0=powerEEG_pcut;
+        all_rem=zeros(1,length(vissymbplot));
+        all_rem(find(vissymbplot=='rem'))=1;
+        artndxn_cut_0=all_rem;
+        artefactfreenr=find(artndxn_cut_0);
+        SWA=squeeze(mean(powerEEG_pcut_0(1,5:19,artefactfreenr),2));   
+        
+        SWA_filled_0=nan(1,18900);
+        SWA_filled_0(artefactfreenr)=SWA;
+        STD_0=vissymb_cut;
+        STD_0(end+1:18900)='m';
+            
+        % BL1 missing      
+        SWA_filled_1=nan(1,24300); %27 h
+        STD_1=repelem('m',24300); %27 h
+        
+    else
+        
+        cd(datapath0)
+        filenames0=dir('*power*.mat')
+        filename=filenames0(i).name;
+        load(filename);    
+        powerEEG_pcut_0=powerEEG_pcut;
+        all_rem=zeros(1,length(vissymbplot));
+        all_rem(find(vissymbplot=='rem'))=1;
+        artndxn_cut_0=all_rem;
+        artefactfreenr=find(artndxn_cut_0);
+        SWA=squeeze(mean(powerEEG_pcut_0(1,5:19,artefactfreenr),2));       
+        
+        %shift data according to starttime
+        if i==4 || i==7 || i==9 || i==10 %starttime 14h
+            SWA_filled_0=nan(1,18900);
+            SWA_filled_0(artefactfreenr)=SWA;
+            STD_0=vissymb_cut;
+            STD_0(end+1:18900)='w';
+        elseif i==2 || i==5 %starttime 15.2h
+            SWA_filled_0=nan(1,18900);
+            SWA_filled_0(artefactfreenr+1080)=SWA;
+            STD_0=repelem('m',18900); %21 h
+            STD_0(1081:length(vissymb_cut)+1080)=vissymb_cut;
+            STD_0(end+1:18900)='w';
+        elseif i==3 %starttime 13h
+            SWA_filled_00=nan(1,19800);
+            SWA_filled_00(artefactfreenr)=SWA;
+            SWA_filled_0=SWA_filled_00(901:19800);
+            STD_0=vissymb_cut(901:19800);
+        elseif i==11 %starttime 12h (only 2 hours recording, rest missing)
+            SWA_filled_00=nan(1,20700);
+            SWA_filled_00(artefactfreenr)=SWA;
+            SWA_filled_0=SWA_filled_00(1801:20700);
+            STD_0=vissymb_cut(1801:end);
+            STD_0(end+1:18900)='m';
+        elseif i==6 || i==8 %starttime 14.5h
+            SWA_filled_0=nan(1,18900);
+            SWA_filled_0(artefactfreenr+450)=SWA;
+            STD_0=repelem('m',18900); %21 h
+            STD_0(451:length(vissymb_cut)+450)=vissymb_cut;
+            STD_0(end+1:18900)='w';
+            if i==6
+            STD_0(11028:11089)='m';
+            else
+            end
+        end
+        
+      % Baseline 1        
+        cd(datapath1)
+        filenames1=dir('*power*.mat')
+        filename=filenames1(i-1).name;
+        load(filename);       
+        powerEEG_pcut_1=powerEEG_pcut;
+        all_rem=zeros(1,length(vissymbplot));
+        all_rem(find(vissymbplot=='rem'))=1;
+        artndxn_cut_1=all_rem;
+        artefactfreenr=find(artndxn_cut_1);
+        SWA=squeeze(mean(powerEEG_pcut_1(1,5:19,artefactfreenr),2));       
+        SWA_filled_1=nan(1,24300);
+        SWA_filled_1(artefactfreenr)=SWA;
+        STD_1=vissymb_cut;
+        STD_1(end+1:24300)='w';
+    end
+      % Sleep Restriction and Baseline 2     
+        cd(datapath2)
+        filename=filenames2(i).name;
+        load(filename);
+        powerEEG_pcut_2=powerEEG_pcut;
+        all_rem=zeros(1,length(vissymbplot));
+        all_rem(find(vissymbplot=='rem'))=1;
+        artndxn_cut_2=all_rem;
+        artefactfreenr=find(artndxn_cut_2);
+        SWA=squeeze(mean(powerEEG_pcut_2(1,5:19,artefactfreenr),2));       
+        SWA_filled_2=nan(1,38700); %43 h
+        SWA_filled_2(artefactfreenr)=SWA;
+        STD_2=vissymb_cut;
+        STD_2(end+1:38700)='w';
+        
+      % Put together and normalize with Baseline (5:00 - 5:00)
+        SWA_filled_012=cat(2,SWA_filled_0,SWA_filled_1,SWA_filled_2);
+        SWA_BL = SWA_filled_2(15*60*15+1:39*60*15); % SWA of baseline (5:00 - 5:00, starting 15 hours after start of 2nd recording)
+        SWA_filled_normalized=SWA_filled_012/nanmean(SWA_BL);  % normalize with BL 2 (5:00 - 5:00)
+        STD=horzcat(STD_0,STD_1,STD_2);
+
+        SWA_absolute(i,:) = SWA_filled_012;
+        SWA_normalized(i,:) = SWA_filled_normalized;
+        STD_all(i,:)=STD;
+        filenames{i}=filenames2(i).name(1:8);
+        
+        clear powerEEG_pcut* artndxn_cut* artefactfreenr SWA SWA_filled* STD_0 STD_1 STD_2
+        
+end
+
+% add known wake epochs to scoring(first sleep restriction RD2-Dec (ID1), feeding
+% times RD-2 July (ID2) and RD-3-July (ID5)
+scoring=STD_all;
+scoring(1,46*60*15+1:49*60*15) = 'w'; % 12:00 - 14:00 (first sleep restriction)
+scoring(2,19*60*15+151:21*60*15+75) = 'w'; % 9:10 - 11:05 (according to protocol)
+scoring(5,19*60*15+151:21*60*15+75) = 'w'; % 9:10 - 11:05 (according to protocol)
+
+
+%episodes
+
+cd('C:\Users\schlaf\Documents\reindeer\Data_Analysis_main_experiment\Results\episodes\all')
+load('episodes.mat')
+
+  %% save
+  
+cd(savepath)
+save('SWA_scoring_episodes_rem','SWA_normalized','SWA_absolute','scoring','filenames','episodes')
+close all  
+clear
+
+
+%% plot to check if everything is fine
+
+SWA_rem=SWA_absolute;
+
+load('SWA_scoring_episodes.mat')
+
+%check if it gives same result for nrem sleep as SWA_normalized
+
+for i = 1:11
+    
+    SWA_nrem_norm(i,:) = SWA_abslolute(i,:)/nanmean(SWA_absolute(i,63*900+1:87*900));
+
+end
+
+
+%normalize SWA rem with mean SWA NREM sleep
+
+for i = 1:11
+    
+    SWA_rem_norm(i,:) = SWA_rem(i,:)/nanmean(SWA_absolute(i,63*900+1:87*900));
+
+end
+
+for i = 1:11
+    
+minimum_SWA(i) = prctile(SWA_rem_norm(i,:),1)
+
+end
+
+L = mean(minimum_SWA);
+
+
+for i = 1:11
+    
+maximum_SWA(i) = max(rmoutliers(SWA_nrem_norm(i,:)))
+
+end
+
+
+
+
